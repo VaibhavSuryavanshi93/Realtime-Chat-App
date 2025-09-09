74356c63ccd7faea661bfdf7ca096a1580cb8c8c
Vaibhav Suryavanshi <128031884+VaibhavSuryavanshi93@users.noreply.github.com>
2025-09-09 18:17:50 +0530

    Fix case-sensitive import for MessageInput

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,7 @@
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
-import MessageInput from "./MessageInput";
+import MessageInput from "./MessageInput.jsx";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/userAuthStore";
 import { formatMessageTime } from "../lib/utils";
