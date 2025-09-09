c11642e5f491f3a36067709bfc64c0160a6ad960
Vaibhav Suryavanshi <128031884+VaibhavSuryavanshi93@users.noreply.github.com>
2025-09-09 18:25:32 +0530

    fix error

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,7 @@
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
-import MessageInput from "./MessageInput.jsx";
+import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/userAuthStore";
 import { formatMessageTime } from "../lib/utils";
